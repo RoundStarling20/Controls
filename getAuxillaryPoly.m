a1a6b56e658b3b5d180f193c4135431a70edc8fc
RoundStarling20 <88243850+RoundStarling20@users.noreply.github.com>
2022-03-01 15:45:06 -0700

    Update getAuxillaryPoly.m
    if s things and powers were different sizes, issues could arise

--- getAuxillaryPoly.m
+++ getAuxillaryPoly.m
@@ -1,7 +1,7 @@
 function [row] = getAuxillaryPoly(values,inputLength,columnLength,i)
 % Written By: RoundStarling20
 %    Created: Febuary 27 2022
-%   Modified: Febuary 28 2022
+%   Modified: March 01 2022
 %
 %
 %  Function Description:  
@@ -20,11 +20,17 @@ function [row] = getAuxillaryPoly(values,inputLength,columnLength,i)
 %
 
 syms s
-symbolic = sym(zeros(1,columnLength));
+sThings = sym(zeros(1,columnLength));
 power = inputLength - i + 1;
-sThings = s.^(power:-2:0);
-symbolic(1:length(sThings)) = sThings(1,:);
-auxilaryPolynomial = symbolic.* values(i-1,:);
+for j = 1:columnLength
+    sThings(j) = s.^(power);
+    if power <= 0
+        break;
+    else
+        power = power - 2;
+    end
+end
+auxilaryPolynomial = sThings.* values(i-1,:);
 fprintf('Auxlilary polynomial in row %d: ',i-1);
 disp(sum(auxilaryPolynomial))
 row = subs(diff(auxilaryPolynomial,s),1);
